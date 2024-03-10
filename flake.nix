43e6f05360562e8f28cd61aa38d1cc7b3e8963b3
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2024-03-10 15:09:44 +0200

    EAS

--- flake.nix
+++ flake.nix
@@ -1,24 +1,48 @@
 {
 	inputs = {
-		nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+		nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
 		flake-utils.url = github:numtide/flake-utils;
+		devshell = {
+			url = "github:numtide/devshell";
+			inputs = {
+				nixpkgs.follows = "nixpkgs";
+				flake-utils.follows = "flake-utils";
+			};
+		};
+		android-nixpkgs = {
+			url = github:tadfisher/android-nixpkgs/stable;
+			inputs = {
+				nixpkgs.follows = "nixpkgs";
+				flake-utils.follows = "flake-utils";
+				devshell.follows = "devshell";
+			};
+		};
 	};
 
-	outputs = { self, nixpkgs, flake-utils }:
+	outputs = { self, nixpkgs, flake-utils, android-nixpkgs, ... }:
 		flake-utils.lib.eachDefaultSystem (system:
 			let
 				pkgs = nixpkgs.legacyPackages.${system};
 				node = pkgs.nodejs-18_x;
 				yarn-berry = (pkgs.yarn-berry.override { nodejs = node; });
+				android-sdk = android-nixpkgs.sdk.${system} (sdkPkgs: with sdkPkgs; [
+					cmdline-tools-latest
+					build-tools-34-0-0
+					platform-tools
+					platforms-android-34
+					emulator
+				]);
 			in
 			{
 				devShell = pkgs.mkShell {
 					packages = [
 						node
 						yarn-berry
+						android-sdk
 					];
 
 					shellHook = ''
+						export ANDROID_HOME=${android-sdk}/share/android-sdk
 						yarn install
 					'';
 				};
